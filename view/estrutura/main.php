836ca101501a2269d952ecdc1f93dd2ee8ee0ac7
cleisson.heggdorne <ELadmin@WKS-ATC194-DGM.el.com.br>
2022-04-03 08:28:23 -0300

    cadastro fornecedor

--- view/estrutura/main.php
+++ view/estrutura/main.php
@@ -10,11 +10,25 @@ require "cabecalho.php";
                 <div class="nav-wrapper">
                 <a href="#" class="brand-logo right">Controle</a>
                 <a href="#" data-target="mobile-demo" class="sidenav-trigger left"><i class="material-icons">menu</i></a>
-                    <ul id="nav-mobile" class="left hide-on-med-and-down">
-                        <li><a href="#">Produtos</a></li>
-                        <li><a href="#">Fornecedores</a></li>
-                        <li><a href="#">Transportadoras</a></li>
+                    <ul class="tabs left hide-on-med-and-down">
+                        <li class="tab"><a href="#fornecedores">Fornecedores</a></li>
+                        <li class="tab"><a href="#produtos">Produtos</a></li>
+                        <li class="tab"><a href="#transportadoras">Transportadoras</a></li>
                     </ul>
+                        <div id="fornecedores" class="col s12">
+                            <p class='card-panel red lighten-4'>
+                                <a class="modal-trigger waves-effect waves-light btn" href="#novo-forn">Novo</a></p> 
+                        </div>
+                        <div id="produtos" class="col s12">
+                            <p class='card-panel red lighten-4'>
+                                <a class="modal-trigger waves-effect waves-light btn" href="#novo-prod">Novo</a></p>
+                        </div>
+                        <div id="transportadoras" class="col s12"> 
+                            <p class='card-panel red lighten-4'>
+                                <a class="modal-trigger waves-effect waves-light btn" href="#novo-transp">Novo</a></p>
+                        </div> 
+                        
+                    </div>
                 </div>
             </nav>
 
@@ -27,7 +41,108 @@ require "cabecalho.php";
        
 
     <main>
-        
+        <!-- Modal Cadastrar Fornecedor  -->
+    <div class="row">
+            <!-- Modal Structure -->
+            <div id="novo-forn" class="modal modal-fixed-footer">
+                
+                <div class="modal-content">
+
+                    <div class="card-content white-text">
+                        <span class="card-title black-text">Cadastrar Fornecedor</span>
+
+                        <form method="POST">
+                            <input name="id" id="id-edt" type="hidden" value=""></input>
+                            <div class="row">
+                                <div class="col s12 m12 l12">
+                                    <div class="col s6 m6 l6">
+                                        <!--input do nome -->
+                                        <div class="row">
+                                            <div class="input-field col s12">
+                                                <input placeholder="" id="nome" type="text" class="validate" name="nome" required>
+                                                <label for="nome">Nome</label>
+                                            </div>
+                                        </div>
+
+                                        <!--input da descrição -->
+                                        <div class="row">
+                                            <div class="input-field col s12">
+                                                <textarea placeholder="" id="descricao" class="materialize-textarea" name="descricao" required></textarea>
+                                                <label for="descricao">Descrição</label>
+                                            </div>
+                                        </div>
+
+                                        <!--input da Cidade-->
+                                        <div class="row">
+                                            <div class="input-field col s12">
+                                                <input placeholder="" id="cidade" type="text" name="cidade" required>
+                                                <label for="cidade">Cidade</label>
+                                            </div>
+                                        </div>
+                                        <!--input do Endereco -->
+                                        <div class="row">
+                                            <div class="input-field col s12">
+                                                <input placeholder="" id="endereco" type="text" class="validate" name="endereco" required>
+                                                <label for="endereco">Endereço</label>
+                                            </div>
+                                        </div>
+
+                                    </div>
+
+                                    <div class="col s6 m6 l6">
+
+                                        <!--input do bairro -->
+                                        <div class="row">
+                                            <div class="input-field col s12">
+                                                <input placeholder="" id="bairro" type="text" class="validate" name="bairro">
+                                                <label for="bairro">Bairro</label>
+                                            </div>
+                                        </div>
+                                     
+                                        <!--input do número-->
+                                        <div class="row">
+                                            <div class="input-field col s5">
+                                                <input placeholder="" id="numero" type="number" step="1" min=0 class="validate" value="" name="numero">
+                                                <label for="nota-edt">Número</label>
+                                            </div>
+                                            <div class="input-field col s5">
+                                            <p>
+                                                <label>
+                                                    <input type="checkbox" />
+                                                    <span>SN</span>
+                                                </label>
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <!--input do numero telefone -->
+                                        <div class="row">
+                                            <div class="input-field col s3">
+                                                <input placeholder="" id="ddd" value="" type="text" class="validate" name="ddd">
+                                                <label for="ddd">DDD</label>
+                                            </div>
+                                            <div class="input-field col s9">
+                                                <input placeholder="" id="numero-tel" value="" type="text" class="validate" require name="numero-tel">
+                                                <label for="numero-tel">Número Tel</label>
+                                            </div>
+                                        </div>
+                                        <!--input do email -->
+                                        <div class="row">
+                                            <div class="input-field col s12">
+                                                <input placeholder="" id="email" type="text" class="validate" name="email">
+                                                <label for="email">E-mail</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="waves-effect waves-light btn purple">Salvar Alterações</a>
+                </div>
+                </form>
+            </div>
+        </div>
     </main>
     <!--JavaScript at end of body for optimized loading-->
     <script type="text/javascript" src="js/materialize.min.js"></script>
